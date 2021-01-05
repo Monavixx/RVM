989777b7fe2cc73b2f6e8b9c5c1931501d80c3fd
AnatarX <flameelite88@mail.ru>
2021-01-05 19:01:32 +0300

    s

--- TestGenerateCompileFile/main.cpp
+++ TestGenerateCompileFile/main.cpp
@@ -9,7 +9,7 @@ int main()
 
     ds << quint8(1) << QString("NameMainClass").toUtf8();
     ds << quint8(4) << quint8(1) << quint8(1) << QString("void").toUtf8() << QString("NameMainClass").toUtf8() << QString("Main").toUtf8() << int(0) << int(136);
-    ds << quint8(7) << QString("Hello world").toUtf8();
+    ds << quint8(7) << QString("Hello ñorld").toUtf8();
     ds << quint8(2) << int(1) << quint8(1) << QString("Relax.String").toUtf8() << int(1) << QString("Relax.String").toUtf8() << QString("data").toUtf8();
     ds << quint8(6) << int(1);
     ds << quint8(5) << quint8(1) << quint8(1) << QString("void").toUtf8() << QString("Relax.Console").toUtf8() << QString("Write").toUtf8() << int(1) << QString("Relax.String").toUtf8() << QString("text").toUtf8();
