f53625b82fd470852f8331069bff1e7c3b0783f5
Anatar <flameelite88@mail.ru>
2021-01-06 22:14:05 +0300

    commit

--- UnitTestTest/TTestClass.cpp
+++
@@ -1,1 +0,0 @@
-#include "TTestClass.h"
--- UnitTetsLol/Test.h
+++ UnitTetsLol/Test.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <QtCore>
 #include "CppUnitTest.h"
+#include "../RelaxVM/Class.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -9,9 +10,24 @@ namespace UnitTests
     TEST_CLASS(lol)
     {
     public:
-        TEST_METHOD(MyTestFunction)
+        TEST_METHOD(Test_Getters)
         {
-            Assert::IsTrue(true);
+            QString name = "TestClass";
+            MethodSignature methodSignature1("NameMethod", "DataTypeMethod", name, {});
+            MethodSignature methodSignature2("NameMethod2", "DataTypeMethod2", name, {});
+            QList<Method> methods = {
+                Method(methodSignature1),
+                Method(methodSignature2,{}, AccessModifier::PUBLIC, true)
+            };
+            Class testingClass(name, methods);
+
+
+            QString getName = testingClass.GetName();
+            QList<Method> getMethods = testingClass.GetMethods();
+
+
+            Assert::IsTrue(getName == name);
+            Assert::IsTrue(getMethods == methods);
         }
     };
 }
